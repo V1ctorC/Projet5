e5c642519c8d158b0cffb4a54172f4ca12ce4d97
VictorC <victor.du.77@hotmail.fr>
2018-03-23 15:20:48 +0100

    Ajout des differentes routes

--- src/Victor/AdBundle/Controller/AdvertController.php
+++ src/Victor/AdBundle/Controller/AdvertController.php
@@ -11,8 +11,29 @@ class AdvertController extends Controller
     {
         $content = $this
             ->get('templating')
-            ->render('VictorAdBundle:Advert:index.html.twig', array('nom' => 'Victor'));
+            ->render('VictorAdBundle:Advert:index.html.twig', array('nom' => 'Ici carte de la France'));
 
         return new Response($content);
+
+    }
+
+    public function homeAction($departement)
+    {
+        return new Response("Ici liste des annonces concernant le departement numero : " . $departement);
+    }
+
+    public function viewAction($departement ,$id)
+    {
+        return new Response("Ici vu d'une annonce unique de numéro : " . $id . " elle vient du département numéro : " . $departement);
+    }
+
+    public function addAction()
+    {
+        return new Response(("Ici ajout d'une annonce"));
+    }
+
+    public function deleteAction()
+    {
+        return new Response("Ici suppression d'une annonce");
     }
 }

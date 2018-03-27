d71e3e13a2db847cc4c067a1d7e748bddcf2df23
VictorC <victor.du.77@hotmail.fr>
2018-03-27 15:57:33 +0200

    Ajout de quelques annonce de test avec un page pour voir une annonce a l'unite

--- src/Victor/AdBundle/Controller/AdvertController.php
+++ src/Victor/AdBundle/Controller/AdvertController.php
@@ -24,12 +24,39 @@ class AdvertController extends Controller
             throw new NotFoundHttpException('La page ' . $page . ' n\'existe pas.');
         }
 
-        return $this->render('VictorAdBundle:Advert:home.html.twig', array('departement' => $departement, 'page' => $page));
+        $listAdverts = array(
+            array(
+                'title'   => 'Recherche développpeur Symfony',
+                'id'      => 1,
+                'author'  => 'Alexandre',
+                'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+                'date'    => new \Datetime()),
+            array(
+                'title'   => 'Mission de webmaster',
+                'id'      => 2,
+                'author'  => 'Hugo',
+                'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+                'date'    => new \Datetime()),
+            array(
+                'title'   => 'Offre de stage webdesigner',
+                'id'      => 3,
+                'author'  => 'Mathieu',
+                'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+                'date'    => new \Datetime())
+        );
+
+        return $this->render('VictorAdBundle:Advert:home.html.twig', array('listAdverts'=>$listAdverts));
     }
 
-    public function viewAction($departement ,$id)
+    public function viewAction($id)
     {
-        return $this->render('VictorAdBundle:Advert:view.html.twig', array('id' => $id));
+        $advert = array(
+            'title'   => 'Recherche développpeur Symfony',
+            'id'      => 1,
+            'author'  => 'Alexandre',
+            'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+            'date'    => new \Datetime());
+        return $this->render('VictorAdBundle:Advert:view.html.twig', array('advert' => $advert));
     }
 
     public function addAction(Request $request)

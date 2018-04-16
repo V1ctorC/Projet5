d30f14c72febc1991b1adac25cce1ce8077bb260
VictorC <victor.du.77@hotmail.fr>
2018-04-16 17:33:27 +0200

    Ajout de la fonctionnalite permettant de rajouter des telephones direction dans interface admin

--- src/Victor/AdBundle/Controller/AdminController.php
+++ src/Victor/AdBundle/Controller/AdminController.php
@@ -3,16 +3,47 @@
 namespace Victor\AdBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Victor\AdBundle\Entity\Phone;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends Controller
 {
-    public function adminAction()
+    public function adminAction(Request $request)
     {
         $userManager = $this->get('fos_user.user_manager');
 
         $users = $userManager->findUsers();
 
-        return $this->render('@VictorAd/Admin/admin.html.twig', array('users'=>$users));
+        $phone = new Phone();
+
+        $form = $this->get('form.factory')->createBuilder(FormType::class, $phone)
+            ->add('brand',    TextType::class)
+            ->add('model',    TextType::class)
+            ->add('capacity', IntegerType::class)
+            ->add('color',    TextType::class)
+            ->add('save', SubmitType::class)
+            ->getForm();
+
+        if ($request->isMethod('POST'))
+        {
+            $form->handleRequest($request);
+
+            if ($form->isValid())
+            {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($phone);
+                $em->flush();
+
+                return $this->redirectToRoute('victor_core_home');
+            }
+        }
+
+        return $this->render('@VictorAd/Admin/admin.html.twig', array('users'=>$users, 'form' => $form->createView()));
+
     }
 
     public function deleteAction($user)

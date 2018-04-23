b1d9ce775555a3512b5f3bb3cdfef34ff78c6e3e
VictorC <victor.du.77@hotmail.fr>
2018-04-23 13:48:35 +0200

    Durant inscription utilisateur a deux choix pour sexe

--- src/Victor/UserBundle/Form/RegistrationType.php
+++ src/Victor/UserBundle/Form/RegistrationType.php
@@ -10,13 +10,19 @@ namespace Victor\UserBundle\Form;
 
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('gender');
+        $builder->add('gender', ChoiceType::class, array(
+            'choices' => array(
+                'Homme' => 'Homme',
+                'Femme' => 'Femme',
+            ),
+        ));
         $builder->add('firstname');
         $builder->add('lastname');
         $builder->add('address');

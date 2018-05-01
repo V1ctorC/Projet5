480a265da5f285b03bf099b0908cdaae8b5320b4
VictorC <victor.du.77@hotmail.fr>
2018-05-01 13:12:35 +0200

    Petit ajout fichier script.js pour mettre message alors si zipcode ou phone n'est pas valide

---
+++ src/Victor/AdBundle/Resources/public/js/script.js
@@ -0,0 +1,15 @@
+document.getElementById('fos_user_registration_form_zipcode').addEventListener("blur", function (e) {
+    var zipcode = e.target.value;
+    if (zipcode.length !== 5)
+    {
+        alert('Un code postal est composé de 5 chiffres')
+    }
+});
+
+document.getElementById('fos_user_registration_form_phone').addEventListener("blur", function (e) {
+    var phone = e.target.value;
+    if (phone.length !== 10)
+    {
+        alert('Un numéro de téléphone est composé de 10 chiffres')
+    }
+});

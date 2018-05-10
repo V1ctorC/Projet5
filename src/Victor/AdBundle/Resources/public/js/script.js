763728ba39d77dda4cf79c1d8ee504a4e9e42a51
VictorC <victor.du.77@hotmail.fr>
2018-05-10 12:54:23 +0200

    Ajout petite fonctionnalite permettant de mettre mdp en rouge si les mots de passe ne correspondent pas dans le formulaire

--- src/Victor/AdBundle/Resources/public/js/script.js
+++ src/Victor/AdBundle/Resources/public/js/script.js
@@ -14,7 +14,21 @@ document.getElementById('fos_user_registration_form_phone').addEventListener("bl
     }
 });
 
-/*document.getElementById('fos_user_registration_form_email').addEventListener("blur", function (e) {
+document.getElementById('fos_user_registration_form_plainPassword_first').addEventListener("blur", function (e) {
+    var firstmdp = e.target.value;
 
-    console.log(e.target.value);
-});*/
+    document.getElementById('fos_user_registration_form_plainPassword_second').addEventListener("blur", function (ev) {
+        var secondmdp = ev.target.value;
+
+        if (firstmdp === secondmdp)
+        {
+            document.getElementById('fos_user_registration_form_plainPassword_first').style.color = "green";
+            document.getElementById('fos_user_registration_form_plainPassword_second').style.color = "green";
+        }
+        else
+        {
+            document.getElementById('fos_user_registration_form_plainPassword_first').style.color = "red";
+            document.getElementById('fos_user_registration_form_plainPassword_second').style.color = "red";
+        }
+    })
+})

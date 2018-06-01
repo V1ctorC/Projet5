a11ed3775f1a3745cca1bb75d4d2456b106f7b9c
VictorC <victor.du.77@hotmail.fr>
2018-06-01 15:26:38 +0200

    Ajout de la regex phone pour edit form

--- src/Victor/AdBundle/Resources/public/js/editProfile.js
+++ src/Victor/AdBundle/Resources/public/js/editProfile.js
@@ -35,16 +35,20 @@ document.getElementById('fos_user_profile_form_zipcode').addEventListener("blur"
 
 document.getElementById('fos_user_profile_form_phone').addEventListener("blur", function (e) {
     var phone = e.target.value;
-    if (phone.length !== 10)
+    var regexPhone = new RegExp(/^(06|07)[0-9]{8}$/);
+    var phoneNumber = regexPhone.test(phone);
+    if (phoneNumber !== true)
     {
+        alert(phoneNumber);
         document.getElementById('fos_user_profile_form_phone').style.color = "red";
         document.getElementById('fos_user_profile_form_phone').style.border = "2px solid red";
         errorfield.style.display = "block";
         phonefield.style.display = "block";
-        phonefield.innerHTML = "<br/> - Le code numéro de téléphone doit comporter 10 chiffres";
+        phonefield.innerHTML = "<br/> - Le code numéro de téléphone doit comporter 10 chiffres et commencer par 06 ou 07";
     }
     else
     {
+        alert(phoneNumber);
         document.getElementById('fos_user_profile_form_phone').style.color = "inherit";
         document.getElementById('fos_user_profile_form_phone').style.border = "1px solid #ccc";
         phonefield.style.display = "none";
